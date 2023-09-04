357eea0ad8825497f26269c58bb174885ea2c327
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-09-04 10:55:16 +0200

    .

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ function breadCrumbs() {
   console.log("*****");
   document.querySelector("div").innerHTML = `<p><a href="${bc[0].link}">${bc[0].name}</a></p> / <p><a href="${bc[1].link}">${bc[1].name}</a></p> / <p><a href="">${bc[2].name}</a></p>`;
 
+  //   *** evt. lav en if-statement baseret på hvilken HTML-underside man står på og hvis den korrekte sti derfra
+
   //    bc.forEach((element) => {
   //     `<p><a href="${element.link}">${element.name}</a></p>` + "/";
   //   });
